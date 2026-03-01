ff60ea9ca332e3434d8345a89105209dab418689
CoderMohammadShuvo <mohammad1.sunny1@gmail.com>
2026-03-01 06:57:46 +0600

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "z-p3-scontent.fdac3-2.fna.fbcdn.net",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
   experimental: {},
