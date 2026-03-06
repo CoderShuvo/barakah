cee80e2dcec43f71351b5c43d0c8d1cbc6ee0f5d
CoderShuvo <alexshuvrow99@gmail.com>
2026-03-07 01:26:24 +0600

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -79,6 +79,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "youtube.com",
+      },
     ],
   },
   experimental: {},
